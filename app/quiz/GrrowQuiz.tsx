a6cc0567a85970d1ad5bcba3bec92121889b1380
MGhunch <michael@hunch.co.nz>
2025-08-27 06:57:05 +1200

    Update GrrowQuiz.tsx

--- app/quiz/GrrowQuiz.tsx
+++ app/quiz/GrrowQuiz.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useMemo, useState } from 'react';
-import Progress from './quizprogress';
+import Progress from './progress';
 import Breadcrumb from './breadcrumb';
 import ScaleLabels from './scalelabels';
 import { ANCHORS, CIRCLES } from './constants';
