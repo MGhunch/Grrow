e231cd7696faaa70a03f036eb1a714420c377c61
MGhunch <michael@hunch.co.nz>
2025-08-27 05:33:29 +1200

    Create constants.ts

---
+++ app/quiz/constants.ts
@@ -0,0 +1,6 @@
+// Shared constants for the quiz
+
+import type { Circle } from "./types";
+
+export const ANCHORS = ["Not yet", "Sometimes", "Mostly", "Consistently"] as const;
+export const CIRCLES: Circle[] = ["ESSENTIALS", "EXPLORING", "SUPPORTING", "LEADING"];
