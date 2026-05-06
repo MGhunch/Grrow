b42cbf0ae4ec2ec7a59488dcc12411378907da77
MGhunch <michael@hunch.co.nz>
2026-05-07 09:57:44 +1200

    Add files via upload

--- src/components/layout/QuizTrialSection.tsx
+++ src/components/layout/QuizTrialSection.tsx
@@ -5,7 +5,7 @@ import { useTheme } from "../../lib/ThemeContext";
 import { COLORS, whiteAlpha } from "@/lib/colors";
 import { TEXT } from "@/lib/typography";
 import { ButtonPrimary } from "../shared/Button";
-import QuizWrap from "../quiz/QuizWrap";
+import ExplorerEntry from "../quiz/ExplorerEntry";
 
 interface QuizTrialSectionProps {
   headline?: string;
@@ -57,7 +57,7 @@ export default function QuizTrialSection({
       </div>
 
       {showModal && (
-        <QuizWrap dark={dark} onClose={() => setShowModal(false)} />
+        <ExplorerEntry dark={dark} onClose={() => setShowModal(false)} />
       )}
     </>
   );
